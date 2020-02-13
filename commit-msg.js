3b0e45899b5767a863ccd69f83c486e58fda18d1
Mohammad Akram <mohammad.akram@WINDELLAP-388.local>
2020-02-13 19:28:33 +0530

    fdfafasdasdasddsdsdsf

--- commit-msg.js
+++ commit-msg.js
@@ -6,16 +6,19 @@ const execSync = require('child_process').execSync;
 const COMMIT_MSG_FILE = `.git/COMMIT_EDITMSG`;
 const PATTERN = /^([a-zA-Z]*)(\((.+?)\))?\: (.*)$/;
 
+const STATUS_FAIL = 'failure';
+const STATUS_PASS = 'success';
+
 const msg = fs.readFileSync(COMMIT_MSG_FILE, 'utf-8').trim();
 const match = PATTERN.exec(msg);
 if(match) {
   try {
     execSync(`git commit -m ${msg}`, { stdio: [0, 1, 2] });
-    process.emit('exit', 0);
+    process.emit('exit', STATUS_PASS);
   } catch(e) {
-    process.emit('exit', 1);
+    process.emit('exit', STATUS_FAIL);
   }
 }else {
   console.log(chalk.red.inverse('Invalid Commit'));
-    process.emit('exit', 1);
+  process.emit('exit', STATUS_FAIL);
 }
